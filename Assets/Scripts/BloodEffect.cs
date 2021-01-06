9fee109ca78813346483474f488e8940a4e425bd
liukang <2549974870@qq.com>
2021-01-06 18:36:41 +0800

    在次提交

---
+++ Assets/Scripts/BloodEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodEffect : MonoBehaviour
+{
+    public float timeToDestroy;//删除时间
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;//人物位置
+    public float smoothing;//平滑的值
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)//如果不判断人物死掉就要报错
+        {
+            if (target)
+            {
+                if (transform.position  != target.position)
+                {
+                    Vector3 targetPos = target.position;
+                    transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
