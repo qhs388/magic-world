29c4d5cca57d832705619a0b341cce4d51fe6bd6
qhs388 <2549974870@qq.com>
2021-02-01 23:31:36 +0800

    新增回旋镖

---
+++ Assets/Scripts/Weapon/Sickle.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sickle : MonoBehaviour
+{
+
+    public float speed;
+    public int damage;
+    public float rotateSpeed;
+    public float tuning;
+
+    private Rigidbody2D rb2d;
+    private Transform playerTransfrom;//玩家位置
+    private Transform sickleTransfrom;//回旋镖位置
+    private Vector2 startSpeed;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = transform.right * speed;
+        startSpeed = rb2d.velocity;
+        playerTransfrom = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        sickleTransfrom = GetComponent<Transform>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotateSpeed);//让回旋镖旋转
+
+        float y = Mathf.Lerp(transform.position.y, playerTransfrom.position.y, tuning);
+
+        transform.position = new Vector3(transform.position.x, y, 0);
+        rb2d.velocity = rb2d.velocity - startSpeed * Time.deltaTime;
+
+        if (Mathf.Abs(transform.position.x - playerTransfrom.position.x) <0.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+
+        {
+            other.GetComponent<Enemy>().TakeDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon/SickleHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SickleHit : MonoBehaviour
+{
+    public GameObject Sickle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(Sickle, transform.position, transform.rotation);
+    }
+}
